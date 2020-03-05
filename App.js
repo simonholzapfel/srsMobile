544e0ee4d33263934c026a84e2a725515ee9ca27
Simon Holzapfel <simon.holzapfel@gmx.at>
2020-03-05 17:59:20 +0100

    [+] Added Header

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
 import React, {useState} from 'react';
-import { StyleSheet, FlatList} from 'react-native';
-
-const styles = StyleSheet.create({
-  rootView: {
-    backgroundColor: '#f5f5f1',
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'stretch'
-  },
-  MainMenu: {
-    flex: 2
-  },
-  TopBar: {
-  }
-});
+import { StyleSheet, FlatList, ScrollView, View} from 'react-native';
+import Header from './components/Header'
+import ContinueDeck from './components/ContinueDeck';
 
 //background #f5f5f1
 //background2 #DBE2E9
 //dark #465062
 //green #26d07c
 
-export default function App() {
+const App = () => {
   return (
-    <FlatList />
+    <View style={styles.screen}>
+      <Header title="SRS"/>
+      <ContinueDeck />
+    </View>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: '#f5f5f1',
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'stretch'
+  }
+});
+
+export default App;

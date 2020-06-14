4babf1d95140a026dc2c8ff1643029bf68c7d9a5
Simon Holzapfel <simon.holzapfel@gmx.at>
2020-06-14 22:52:03 +0200

    added additional demo decks

--- data/dummy.js
+++ data/dummy.js
@@ -61,8 +61,118 @@ const tinfDeck = {
             ]
         },
     ]
-}
+};
+
+const bitDeck = {
+    name: "Bits & Bytes",
+    thumbnail: require("../assets/bits_bytes.jpg"),
+    cards: [
+        {
+            front: [
+                {
+                    type: "text",
+                    text: "How many bits has one byte?"
+                }
+            ],
+            back: [
+                {
+                    type: "text",
+                    text: "One byte has 8 bits"
+                }
+            ]
+        },
+    ]
+};
+
+const frenchDeck = {
+    name: "French Vocabulary",
+    thumbnail: require("../assets/french_flag.jpeg"),
+    cards: [
+        {
+            front: [
+                {
+                    type: "text",
+                    style: {
+
+                    },
+                    text: "Forest"
+                }
+            ],
+            back: [
+                {
+                    type: "text",
+                    style: {
+
+                    },
+                    text: "Forêt"
+                }
+            ]
+        },
+        {
+            front: [
+                {
+                    type: "text",
+                    style: {
+
+                    },
+                    text: "Vogel"
+                }
+            ],
+            back: [
+                {
+                    type: "text",
+                    style: {
+
+                    },
+                    text: "Oiseau"
+                }
+            ]
+        },
+        {
+            front: [
+                {
+                    type: "text",
+                    style: {
+
+                    },
+                    text: "Katze"
+                }
+            ],
+            back: [
+                {
+                    type: "text",
+                    style: {
+
+                    },
+                    text: "chat"
+                }
+            ]
+        },
+        {
+            front: [
+                {
+                    type: "text",
+                    style: {
+
+                    },
+                    text: "Panzer"
+                }
+            ],
+            back: [
+                {
+                    type: "text",
+                    style: {
+
+                    },
+                    text: "réservoir"
+                }
+            ]
+        },
+    ]
+};
 
 export const demoDecks = [
     tinfDeck,
+    frenchDeck,
+    bitDeck,
 ]
--- demo/DemoScreen.js
+++ demo/DemoScreen.js
@@ -1,17 +1,15 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Button, } from 'react-native';
 import Card from '../components/Card'
-import {demoDecks} from '../data/dummy';
+import { demoDecks } from '../data/dummy';
 import DemoDeckOverview from './DemoDeckOverview';
 
 const DemoScreen = props => {
     return (
         <View style={{ ...styles.root, ...props.style }}>
-            <DemoDeckOverview navigation={props.navigation} style={styles.overview}/>
+            <DemoDeckOverview navigation={props.navigation} style={styles.overview} />
         </View>
     )
-
-    //<Card data={demoDecks[0].cards[0]} deckHandler={console.log}/>
 };
 
 const styles = StyleSheet.create({
@@ -20,7 +18,7 @@ const styles = StyleSheet.create({
     },
     overview: {
         flex: 1
-    },  
+    },
 });
 
 export default DemoScreen;
